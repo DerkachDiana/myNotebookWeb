cf059f7322c9644b9f68264de4b82b05e2fa8c1a
qwerty <qwerty@mail.ru>
2022-12-08 19:42:33 +0300

    Get rid of initial files

--- src/constants/textType/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-import { Colors } from '../colors';
-
-export const TextStyle = styled.Text`
-  font-size: ${(props) => props.textType.size};
-  color: ${Colors.WHITE};
-  font-family: 'playfair_display_regular';
-  `;
